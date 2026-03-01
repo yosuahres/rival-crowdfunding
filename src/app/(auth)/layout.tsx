bc545dda366e34c53a6702f9191af2c004caa47e
yosuahres <haresyosuaa@gmail.com>
2026-03-01 23:06:52 +0700

    fix: solve lint and checks

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,3 @@
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
